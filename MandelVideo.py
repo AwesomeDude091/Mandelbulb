344482155206e8aceee2c788e62d7c1155962e6b
AwesomeDude091 <computer.cubegeek4life@gmail.com>
2022-10-26 22:55:50 +0200

    More Improvements

--- MandelVideo.py
+++ MandelVideo.py
@@ -1,10 +1,13 @@
-import os
 import cv2
+import numpy
+
+from Mandelbrot import Mandelbrot
 
 
 class MandelVideo:
 
-    def __init__(self, start_left_x, start_right_x, start_y, end_left_x, end_right_x, end_y, fps: int, duration: int, cores):
+    def __init__(self, start_left_x, start_right_x, start_y, end_left_x, end_right_x, end_y, fps: int, duration: int,
+                 log: bool, cores):
         self.start_left_x = start_left_x
         self.start_right_x = start_right_x
         self.start_y = start_y
@@ -15,6 +18,7 @@ class MandelVideo:
         self.end_right_y = end_y + ((self.end_right_x - end_left_x)/16)*9
         self.fps: int = fps
         self.duration: int = duration
+        self.log: bool = log
         self.cores = cores
         # print(self.start_left_x, self.start_right_x, self.start_y, self.start_right_y)
         # print(end_left_x, self.end_right_x, end_y, self.end_right_y)
@@ -24,18 +28,25 @@ class MandelVideo:
         writer = cv2.VideoWriter('mandel.mp4', fourcc, self.fps, (3840, 2160))
         bread = Mandelbrot(3840, 2160, self.cores)
         total_frames = (self.fps * self.duration)
-        progression_left_x = (self.end_x - self.start_left_x) / (total_frames - 1)
-        progression_right_x = (self.end_right_x - self.start_right_x) / (total_frames - 1)
-        progression_left_y = (self.end_y - self.start_y) / (total_frames - 1)
-        # progression_right_y = (self.end_right_y - self.start_right_y) / (total_frames - 1)
-        # self.start_right_y + (i * progression_right_y)
+        if self.log:
+            progression_left_x = (self.end_x - self.start_left_x) / (numpy.log(total_frames))
+            progression_right_x = (self.end_right_x - self.start_right_x) / (numpy.log(total_frames))
+            progression_left_y = (self.end_y - self.start_y) / (numpy.log(total_frames))
+        else:
+            progression_left_x = (self.end_x - self.start_left_x) / (total_frames - 1)
+            progression_right_x = (self.end_right_x - self.start_right_x) / (total_frames - 1)
+            progression_left_y = (self.end_y - self.start_y) / (total_frames - 1)
         for i in range(0, total_frames):
             progress = i * 100 / total_frames
             print("Progress: " + str(progress) + " %")
-            current_left = self.start_left_x + (i * progression_left_x)
-            current_right = self.start_right_x + (i * progression_right_x)
-            current_bottom = self.start_y + (i * progression_left_y)
-            # print(current_left, current_right, current_bottom, current_top)
+            if self.log:
+                current_left = progression_left_x * numpy.log(i + 1) + self.start_left_x
+                current_right = progression_right_x * numpy.log(i + 1) + self.start_right_x
+                current_bottom = progression_left_y * numpy.log(i + 1) + self.start_y
+            else:
+                current_left = self.start_left_x + (i * progression_left_x)
+                current_right = self.start_right_x + (i * progression_right_x)
+                current_bottom = self.start_y + (i * progression_left_y)
             bread.generate_image(current_left, current_right, current_bottom)
             writer.write(cv2.flip(cv2.cvtColor(bread.get_image_array(), cv2.COLOR_RGB2BGR), 0))
 
@@ -43,8 +54,7 @@ class MandelVideo:
 
 
 if __name__ == '__main__':
-    os.environ['CUDA_PATH'] = 'C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.8'
-    from Mandelbrot import Mandelbrot
     # Mandelbrot(3840, 2160, 16).generate_image(-0.04491, -0.04486, 0.98261)
-    Mandelbrot(3840, 2160, 16).generate_image(-8 / 3, 8 / 3, -1.5)
-    # MandelVideo(-(8/3), 8/3, -1.5, -0.04491, -0.04486, 0.98261, fps=24, duration=60, cores=16).generate_video()
+    # Mandelbrot(3840, 2160, 16).generate_image(-8 / 3, 8 / 3, -1.5)
+    MandelVideo(-(8/3), 8/3, -1.5, -0.04491, -0.04486, 0.98261, fps=24, duration=30, log=True, cores=16)\
+        .generate_video()
--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,12 +1,10 @@
+import os
+import time
 from os import getenv
 
 import PIL.ImageColor
 import cv2
 import numpy
-import pycuda.driver as cuda
-import pycuda.autoinit
-import pycuda.gpuarray as gpuarray
-from pycuda.compiler import SourceModule
 import numpy as np
 import multiprocessing as mp
 
@@ -57,9 +55,8 @@ class Mandelbrot:
                 imag = start_y + (y_iteration * y)
                 t = self.is_out_of_bounds(complex(real, imag))
                 if t != 0:
-                    colorIndex = t % len(self.colors)
+                    colorIndex = int((int(t)/8) % len(self.colors))
                     color = PIL.ImageColor.getrgb(self.colors[colorIndex])
-                    print(color)
                     array[y, x] = color
                 else:
                     color = PIL.ImageColor.getrgb('black')
@@ -80,76 +77,82 @@ class Mandelbrot:
         cv2.imwrite('test.jpg', img)
 
     def generate_gpu_image(self, start_x: float, end_x: float, start_y: float):
+        os.environ['CUDA_PATH'] = 'C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.8'
         start_x, end_x, start_y, end_y = self.calc_window(start_x, end_x, start_y)
         self.generate_gpu_array(start_x, end_x, start_y, end_y)
         img = cv2.flip(cv2.cvtColor(self.get_image_array(), cv2.COLOR_RGB2BGR), 0)
-        cv2.imwrite('test.jpg', img)
+        cv2.imwrite('test_gpu.jpg', img)
 
     def generate_gpu_array(self, start_x: float, end_x: float, start_y: float, end_y: float):
         self.array = np.zeros(shape=(self.image_height, self.image_width, 3), dtype=np.uint8)
         width = end_x - start_x
         height = end_y - start_y
-        pool = mp.Pool(self.cores)
         x_iteration = width / self.image_width
         y_iteration = height / self.image_height
-        x_interval = self.image_width
-        y_interval = int(self.image_height / self.cores)
-        result = {}
-        for i in range(0, self.cores):
-            subarray = self.array[(i * y_interval):((i + 1) * y_interval), 0:x_interval]
-            result[i] = pool.apply_async(self.calc_points, [subarray, start_x, start_y + (y_iteration * i * y_interval),
-                                                            x_iteration, y_iteration, y_interval, 0])
-
-        final_array = result[0].get()
-        for j in range(1, self.cores):
-            partial_array = result[j].get()
-            final_array = np.vstack((final_array, partial_array))
-
-        self.array = final_array
-        pool.close()
+        self.array = self.cuda_points(self.array, start_x, start_y, x_iteration, y_iteration, self.image_height, 0)
 
     def cuda_points(self, array, start_x, start_y, x_iteration, y_iteration, y_max, y_min):
+        import pycuda.driver as cuda
+        import pycuda.autoinit
+        import pycuda.gpuarray as gpuarray
+        from pycuda.compiler import SourceModule
+        if os.system("cl.exe"):
+            os.environ[
+                'PATH'] += ';' + r"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.33.31629\bin\Hostx86\x64"
+        if os.system("cl.exe"):
+            raise RuntimeError("cl.exe still not found, path probably incorrect")
         shape = array.shape
         image_rgb = gpuarray.empty(shape, dtype=gpuarray.vec.uchar4)
         cuda.memcpy_htod(image_rgb.gpudata, array.data)
         fractal = gpuarray.empty(shape, dtype=np.uint8)
         mod = SourceModule("""
-            #include <complex>
-            __global__ void bulb(const uchar4* const rgbImage, const unsigned uchar4* const fractal,
-                             float start_x, float start_y, float x_iteration, float y_iteration,
-                             int y_min, int y_max, int image_width)
-            {
-                for (int x = 0; x < image_width; x++) {
-                    float real = start_x + (x_iteration * x);
-                    for (int y = y_min; y < y_max; y++) {
-                        float imag = start_y + (y_iteration * y);
-                        complex<double> mycomplex(real, imag);
-                        int t = is_out_of_bounds(mycomplex);
-                        if (t == 0) {
-                            
+            #include <thrust/complex.h>
+            
+            extern "C" {
+                __device__ int is_out_of_bounds(thrust::complex<float> c) 
+                {
+                    thrust::complex<float> z = thrust::complex<float>(0.0f, 0.0f);
+                    int runs = 0;
+                    for (int i = 0; i < 256; i++) {
+                        z = z * z + c;
+                        runs = i;
+                        if (thrust::abs(z) > 1000) {
+                            break;
                         }
                     }
-                }
-            }
-            
-            __global__ int is_out_of_bounds(complex<double> mycomplex) {
-                complex<double> z(0,0);
-                int t = 0
-                for (int i = 0; i < 256; i++) {
-                    z = z * z + mycomplex
-                    t = i
-                    if (abs(z) > 1000) {
-                        break;
+                    if (thrust::abs(z) > 2) {
+                        return runs;
                     }
+                    return 0;
                 }
-                if (abs(z) > 2) {
-                    return t
+                
+                __global__ void bulb (const uchar4* const rgbImage, uchar4* const fractal, int numRows, int numCols,
+                                 float start_x, float start_y, float x_iteration, float y_iteration,
+                                 int y_min, int y_max, int image_width)
+                {
+                    int x = threadIdx.x + blockIdx.x * blockDim.x;
+                    int y = threadIdx.y + blockIdx.y * blockDim.y;
+                    if (y < numCols && x < numRows) {
+                        int index = numRows*y + x;
+                        float real = start_x + (x_iteration * x);
+                        float imag = start_y + (y_iteration * y);
+                        thrust::complex<float> c = thrust::complex<float>(real, imag);
+                        int runs = is_out_of_bounds(c);
+                        uchar4 color;
+                        if (runs == 0) {
+                            color = make_uchar4(0.0f, 0.0f, 0.0f, 0.0f);
+                        } else {
+                            color = make_uchar4(255.0f, 255.0f, 255.0f, 255.0f);
+                        }
+                        fractal[index] = color;
+                    }    
                 }
-                return 0
-            }
-        """)
-        bulb = mod.get_function("bulb")
-        bulb(image_rgb, fractal, start_x, start_y, x_iteration, y_iteration, y_min, y_max, self.image_width)
+            }    
+        """, no_extern_c=True)
+        func = mod.get_function("bulb")
+        func(image_rgb, fractal, np.int32(shape[0]), np.int32(shape[1]), numpy.float32(start_x), numpy.float32(start_y),
+             numpy.float32(x_iteration), numpy.float32(y_iteration), np.int32(y_min), np.int32(y_max),
+             np.int32(self.image_width), block=(1024, 1, 1))
         return np.array(fractal.get(), dtype=np.uint8)
 
     def get_image_array(self):
@@ -170,6 +173,9 @@ class Mandelbrot:
 
 
 if __name__ == '__main__':
-    Mandelbrot(3840, 2160, 16).generate_image(-0.04491, -0.04486, 0.98261)
+    clock = time.time()
+    Mandelbrot(3840, 2160, 16).generate_gpu_image(-8 / 3, 8 / 3, -1.5)
+    # Mandelbrot(3840, 2160, 16).generate_image(-0.04491, -0.04486, 0.98261)
     # Mandelbrot(3840, 2160, 16).generate_image(-0.08, 0, 0.96)
     # Mandelbrot(3840, 2160, 16).generate_image(-8 / 3, 8 / 3, -1.5)
+    print("Time to complete: {}".format(time.time() - clock))
